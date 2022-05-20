6b60d9702d1d3b2ec129fb4bfc63be709bb1d223
Den0110 <denkov0110@yandex.ru>
2022-05-20 20:12:39 +0300

    bottom bar added

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -3,4 +3,27 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color white = Color.fromRGBO(255, 255, 255, 1);
+
+  static const MaterialColor mainColor = MaterialColor(
+    0xFF08E0C2,
+    <int, Color>{
+      50: Color(0xFF08E0C2),
+      100: Color(0xFF08E0C2),
+      200: Color(0xFF08E0C2),
+      300: Color(0xFF08E0C2),
+      400: Color(0xFF08E0C2),
+      500: Color(0xFF08E0C2),
+      600: Color(0xFF08E0C2),
+      700: Color(0xFF08E0C2),
+      800: Color(0xFF08E0C2),
+      900: Color(0xFF08E0C2),
+    },
+  );
+
+  static const Color lightBlue = Color(0xFF04EBFA);
+  static const Color darkBlue = Color(0xFF03314B);
+  static const Color green = Color(0xFF34AA44);
+  static const Color red = Color(0xFFE6492D);
+  static const Color secondaryText = Color(0x66222222);
+  static const Color iconBg = Color(0x1A000000);
 }
--- lib/main/main_screen.dart
+++ lib/main/main_screen.dart
@@ -1,15 +1,48 @@
+import 'package:camp_app/core/constants/app_colors.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _pageIndex = 0;
+
+  void _onPageSelected(int index) {
+    setState(() {
+      _pageIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         child: Text('main page'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
+        onTap: _onPageSelected,
+        currentIndex: _pageIndex,
+        selectedItemColor: AppColors.mainColor,
+        unselectedItemColor: AppColors.secondaryText,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Главная",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.gamepad),
+            label: "Счета",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.ac_unit),
+            label: "Инвестиции",
+          ),
+        ],
       ),
     );
   }
